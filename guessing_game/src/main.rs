649a038448c1a95551587a1231851ebf14c879c3
boku <zaregototsukai13@gmail.com>
2023-11-13 03:54:14 +0530

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hiii :3, welcome to the guessing game! <3333");
+
+    println!("Please enter a number from 1-10!!!!");
+   
+    let mut guess = String::new();
+
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+    
+    println!("You guessed : {guess}")
+    
 }
