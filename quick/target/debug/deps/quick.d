82dc590f4c9dd50b0819598b83a8ecb099391807
boku <zaregototsukai13@gmail.com>
2023-11-14 00:06:00 +0530

    loops

---
+++ quick/target/debug/deps/quick.d
@@ -0,0 +1,5 @@
+C:\Users\SQREAM\Desktop\rustacean\quick\target\debug\deps\quick.exe: src\main.rs
+
+C:\Users\SQREAM\Desktop\rustacean\quick\target\debug\deps\quick.d: src\main.rs
+
+src\main.rs:
---
+++ quick/target/debug/quick.d
@@ -0,0 +1,1 @@
+C:\Users\SQREAM\Desktop\rustacean\quick\target\debug\quick.exe: C:\Users\SQREAM\Desktop\rustacean\quick\src\main.rs
